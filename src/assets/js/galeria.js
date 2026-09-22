c689b32bac49685e0c16db0a6bbff0a511d569cf
Giovani Manzatto <giovanimanzattofurlan@gmail.com>
2026-09-22 22:24:16 +0000

    deixando pronto para colocar no site

--- src/assets/js/galeria.js
+++ src/assets/js/galeria.js
@@ -14,157 +14,157 @@ const galeria = [
     {
         name: "Ensaio de Turma da monica",
         categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo7.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo7.webp"
+        imgPequena: "../assets/acompanhamento/pequenas/foto7.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/foto7.webp"
     },
     {
-        name: "Ensaio de Páscoa",
+        name: "Ensaio de mecanica",
         categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        imgPequena: "../assets/acompanhamento/pequenas/foto11.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/foto11.webp"
     },
     {
-        name: "Ensaio de Páscoa",
+        name: "Ensaio de mecanica",
         categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        imgPequena: "../assets/acompanhamento/pequenas/foto12.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo12.webp"
     },
     {
-        name: "Ensaio de Páscoa",
+        name: "Ensaio em Familia",
         categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        imgPequena: "../assets/acompanhamento/pequenas/foto13.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/foto13.webp"
     },{
-        name: "Ensaio de Páscoa",
+        name: "Ensaio de Chaves",
         categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        imgPequena: "../assets/acompanhamento/pequenas/foto17.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/foto17.webp"
     },
     {
-        name: "Ensaio de Páscoa",
+        name: "Ensaio de Chaves",
         categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        imgPequena: "../assets/acompanhamento/pequenas/foto18.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/foto18.webp"
     },
     {
-        name: "Ensaio de Páscoa",
+        name: "Ensaio",
         categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        imgPequena: "../assets/acompanhamento/pequenas/foto21.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/foto21.webp"
     },
     {
-        name: "Ensaio de Páscoa",
+        name: "Ensaio de copa",
         categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        imgPequena: "../assets/acompanhamento/pequenas/foto23.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/foto23.webp"
     },
     {
-        name: "Ensaio de Páscoa",
+        name: "Ensaio de copa",
         categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        imgPequena: "../assets/acompanhamento/pequenas/foto24.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/foto24.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "Ensaio",
+        categoria: "Gestante",
+        imgPequena: "../assets/gestante/pequenas/foto1.webp",
+        imgGrandes: "../assets/gestante/grandes/foto1.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "Ensaio",
+        categoria: "gestante",
+        imgPequena: "../assets/gestante/pequenas/foto2.webp",
+        imgGrandes: "../assets/gestante/grandes/foto2.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "Ensaio",
+        categoria: "gestante",
+        imgPequena: "../assets/gestante/pequenas/foto3.webp",
+        imgGrandes: "../assets/gestante/grandes/foto3.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "Ensaio",
+        categoria: "gestante",
+        imgPequena: "../assets/gestante/pequenas/foto4.webp",
+        imgGrandes: "../assets/gestante/grandes/foto4.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "Ensaio",
+        categoria: "gestante",
+        imgPequena: "../assets/gestante/pequenas/foto25.webp",
+        imgGrandes: "../assets/gestante/grandes/foto25.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "Ensaio",
+        categoria: "gestante",
+        imgPequena: "../assets/gestante/pequenas/foto26.webp",
+        imgGrandes: "../assets/gestante/grandes/foto26.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "Ensaio",
+        categoria: "gestante",
+        imgPequena: "../assets/gestante/pequenas/foto27.webp",
+        imgGrandes: "../assets/gestante/grandes/foto27.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "Ensaio",
+        categoria: "gestante",
+        imgPequena: "../assets/gestante/pequenas/foto28.webp",
+        imgGrandes: "../assets/gestante/grandes/foto28.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "newborn",
+        categoria: "newborn",
+        imgPequena: "../assets/newborn/pequenas/foto5.webp",
+        imgGrandes: "../assets/newborn/grandes/foto5.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "newborn",
+        categoria: "newborn",
+        imgPequena: "../assets/newborn/pequenas/foto9.webp",
+        imgGrandes: "../assets/newborn/grandes/foto9.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "newborn",
+        categoria: "newborn",
+        imgPequena: "../assets/newborn/pequenas/foto10.webp",
+        imgGrandes: "../assets/newborn/grandes/foto10.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "newborn",
+        categoria: "newborn",
+        imgPequena: "../assets/newborn/pequenas/foto14.webp",
+        imgGrandes: "../assets/newborn/grandes/foto14.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "newborn",
+        categoria: "newborn",
+        imgPequena: "../assets/newborn/pequenas/foto15.webp",
+        imgGrandes: "../assets/newborn/grandes/foto15.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "newborn",
+        categoria: "newborn",
+        imgPequena: "../assets/newborn/pequenas/foto16.webp",
+        imgGrandes: "../assets/newborn/grandes/foto16.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "newborn",
+        categoria: "newborn",
+        imgPequena: "../assets/newborn/pequenas/foto19.webp",
+        imgGrandes: "../assets/newborn/grandes/foto19.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "newborn",
+        categoria: "newborn",
+        imgPequena: "../assets/newborn/pequenas/foto20.webp",
+        imgGrandes: "../assets/newborn/grandes/foto20.webp"
     },
     {
-        name: "Ensaio de Páscoa",
-        categoria: "Acompanhamento",
-        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
-        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+        name: "newborn",
+        categoria: "newborn",
+        imgPequena: "../assets/newborn/pequenas/foto22.webp",
+        imgGrandes: "../assets/newborn/grandes/foto22.webp"
     }
     
 ]
