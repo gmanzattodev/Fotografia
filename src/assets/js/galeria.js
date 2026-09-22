ce2338fd21b9fa6e8db91a4e1c1a5b472aed55f3
Giovani Manzatto <giovanimanzattofurlan@gmail.com>
2026-09-22 22:08:05 +0000

    melhorando imagens

--- src/assets/js/galeria.js
+++ src/assets/js/galeria.js
@@ -1,8 +1,170 @@
 const galeria = [
     {
-        nome: "",
-        img: "",
-        categoria: "",
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/foto8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/foto8.webp"
+    },
+    {
+        name: "Ensaio de Fazenda",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/foto6.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/foto6.webp"
+    },
+    {
+        name: "Ensaio de Turma da monica",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo7.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo7.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },{
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
+    },
+    {
+        name: "Ensaio de Páscoa",
+        categoria: "Acompanhamento",
+        imgPequena: "../assets/acompanhamento/pequenas/fofo8.webp",
+        imgGrandes: "../assets/acompanhamento/grandes/fofo8.webp"
     }
     
 ]
